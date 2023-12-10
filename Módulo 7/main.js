a19d060a0f859ee150953bf3588fb978ca900fa9
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2023-12-09 21:01:17 -0300

    Adicionando comentários

--- Módulo 7/main.js
+++ Módulo 7/main.js
@@ -1,11 +1,12 @@
 const form = document.getElementById('form-validacao');
 
+/*Vai ficar de olho no botao submit para chamar a função e */
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
+    /*parseFloat para transformar o número digitado em um número flutuante */
     const campoA = parseFloat(document.getElementById('campoA').value);
     const campoB = parseFloat(document.getElementById('campoB').value);
-
+    /*confere se o digitado não for um número para lançar uma mensagem de erro, e também também */
     if (!isNaN(campoA) && !isNaN(campoB) && campoB > campoA) {
         exibirMensagem('Formulário válido!', 'green');
         limparCampos();
@@ -16,7 +17,9 @@ form.addEventListener('submit', function (e) {
 
 function exibirMensagem(mensagem, cor) {
     const mensagemElement = document.querySelector('.mensagem');
+    /*chamada para mudar a mensagem da função */
     mensagemElement.textContent = mensagem;
+    /*chamada para mudar a cor da função */
     mensagemElement.style.color = cor;
 }
 
